0da06d65a7e5b81e0da9d6d5743019ea5783a5ed
netfamilia683-star <netfamilia683@gmail.com>
2025-09-15 17:17:34 -0300

    Create missing App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Federal Associados</h1>
+      <p>Conectividade Sem Limites</p>
+    </div>
+  );
+}
+
+export default App;
